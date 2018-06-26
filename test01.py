53710074f210a4078711ff5d5907130651a0e315
foxdragon <ddroc_me@sina.com>
2018-06-26 13:43:57 +0800

    for测试

--- test01.py
+++ test01.py
@@ -1,6 +1,15 @@
 #python test
-import re
+""" import re
 print("testfile")
 for i in range(5):
     print(i)
-print("测试成功")
+print("测试成功") """
+adct={'apple':15,'banana':20,'pear':35}
+for key,value in adct.items():
+    print(key,':',value)
+
+for key in adct.keys():
+    print(key)
+
+for value in adct.values():
+    print(value)
